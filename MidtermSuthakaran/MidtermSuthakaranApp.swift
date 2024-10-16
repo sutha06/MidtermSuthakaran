87199de175264145483af24f4efe5ff055c5934a
sutha06 <suthakaran.siva0601@gmail.com>
2024-10-16 17:07:29 -0400

    Initial Commit

---
+++ MidtermSuthakaran/MidtermSuthakaranApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermSuthakaranApp.swift
+//  MidtermSuthakaran
+//
+//  Created by Suthakaran Siva on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct MidtermSuthakaranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
